f99a469304b2f1ad9d5f6c16dcc0de1ac41c5be0
Maria Joyce Martins Rabelo <mariarabelo800@gmail.com>
2025-05-03 20:11:00 -0300

    first commit

---
+++ Day 1 - second project.py
@@ -0,0 +1,15 @@
+print("oreinaj ed oir\noiam ed sert\n")
+
+while True:
+    resposta = input("Decifra-me ou te devoro, qual a resposta do enigma?\n")
+    if resposta == "lady gaga":
+        print('não é hoje que terei meu banquete, você acertou!')
+        break
+    elif resposta == "madonna":
+        print('o ano é 2025, meu querido futuro alimento')
+    elif resposta == "copacabana":
+        print("mas quem cantou neste lugar?")
+    else:
+        print('se prepare para virar minha janta, você errou!')
+
+
